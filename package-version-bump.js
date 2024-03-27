a7d06957d948b47a6b0a3dae1dd7766735775ebb
bsrinath4839 <srinath@exidio.co>
2024-03-27 21:23:10 +0530

    v1.0.19

--- package-version-bump.js
+++ package-version-bump.js
@@ -1,19 +1,14 @@
 const { exec } = require("child_process");
-const { promisify } = require("util");
-function bumpVersion(type, message) {
+function bumpVersion(type) {
   return new Promise(async (resolve, reject) => {
     if (type && ["patch", "minor", "major"].includes(type)) {
-      if (message && message.length > 0) {
-        try {
-          await exec(`yarn version --${type}`);
-          await exec(`git add .`);
-          await exec(`git commit -m "${message}"`);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
-      } else {
-        reject(new Error("Invalid message provided!"));
+      try {
+        await exec(`yarn version --${type}`);
+        await exec(`git add .`);
+        await exec(`git commit -m "chore: Updated the package version"`);
+        resolve();
+      } catch (e) {
+        reject(e);
       }
     } else {
       reject(new Error("Invalid version type provided!"));
@@ -23,19 +18,15 @@ function bumpVersion(type, message) {
 
 async function run() {
   try {
-    console.log("Building the app...");
-    const execPromise = promisify(exec);
-    await execPromise("yarn build");
     const readline1 = require("readline").createInterface({
       input: process.stdin,
       output: process.stdout,
     });
     readline1.question(
-      "Select a version type: patch | minor | major and Git message [with : between] ",
-      async (data) => {
-        const [type = "", message = ""] = data.split(": ");
+      "Select a version type: patch | minor | major: ",
+      async (type) => {
         try {
-          await bumpVersion(type, message);
+          await bumpVersion(type);
         } catch (e) {
           console.error(e.message);
         } finally {
