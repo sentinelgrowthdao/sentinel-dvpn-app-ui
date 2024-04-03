bdacaf7bc8afe2502e7a1d27913162c836177399
bsrinath4839 <srinath@exidio.co>
2024-04-03 20:20:21 +0530

    Scrolling stopped when loader is active

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import Navigation from "./Navigation";
 const App = () => {
   const { error, success, loader } = useSelector((state) => state.alerts);
 
+  React.useEffect(() => {
+    const root = document.getElementById("root");
+    if (root) {
+      if (loader.show) {
+        root.style.overflow = "hidden";
+      }
+      if (!loader.show) {
+        root.style.overflow = "auto";
+      }
+    }
+  }, [loader.show]);
   return (
     <>
       <Navigation />
