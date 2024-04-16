814367c4799e07648bfe0633d9fe47ed9d49e59f
bsrinath4839 <srinath@exidio.co>
2024-04-16 19:16:02 +0530

    Removed content from fr.js

--- src/languages/fr.js
+++ src/languages/fr.js
@@ -1,10 +1,3 @@
-const fr = {
-  something_wrong: "Something went wrong...!",
-  error_while_initiating_the_app: "There is an error while initiating the app.",
-  try_again: "Try again",
-  update_available: "Update available...!",
-  your_app_version: "Your app version is {{version}}",
-  latest_app_version: "",
-};
+const fr = {};
 
 export default fr;
